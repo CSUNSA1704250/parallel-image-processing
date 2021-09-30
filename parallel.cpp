2513d9af0015ea85212c0bea2ffd57f3a527d8b7
Jerson ZC <jzunigacoayla@gmail.com>
2021-09-30 13:02:48 -0500

    style: functions should start with a capital letter
    and have mixed case (according to GStyle guide)

--- parallel.cpp
+++ parallel.cpp
@@ -7,7 +7,7 @@
 using namespace cimg_library;
 
 // Calculate the histogram of an image region
-void histogram(const CImg<unsigned char>& image, const int x0, const int y0,
+void Histogram(const CImg<unsigned char>& image, const int x0, const int y0,
                const int x1, const int y1, const int channel,
                std::vector<int>& res) {
   for (int i = x0; i < x1; i++) {
@@ -18,24 +18,24 @@ void histogram(const CImg<unsigned char>& image, const int x0, const int y0,
 }
 
 // Returns pixel intensity on given channel
-std::vector<int> image_analysis(const CImg<unsigned char>& image,
-                                const int channel) {
+std::vector<int> ImageAnalysis(const CImg<unsigned char>& image,
+                               const int channel) {
   std::vector<int> res(256, 0);
 
   const int width = image.width();
   const int height = image.height();
 
   // clang-format off
-  std::thread t1(histogram, std::ref(image),
+  std::thread t1(Histogram, std::ref(image),
                  0, 0, width / 4, height,
                  channel, std::ref(res));
-  std::thread t2(histogram, std::ref(image),
+  std::thread t2(Histogram, std::ref(image),
                  width / 4, 0, (width / 4) * 2, height,
                  channel, std::ref(res));
-  std::thread t3(histogram, std::ref(image),
+  std::thread t3(Histogram, std::ref(image),
                  (width / 4) * 2, 0, (width / 4) * 3, height,
                  channel, std::ref(res));
-  std::thread t4(histogram, std::ref(image),
+  std::thread t4(Histogram, std::ref(image),
                  (width / 4) * 3, 0, width, height,
                  channel, std::ref(res));
   // clang-format on
@@ -51,7 +51,7 @@ std::vector<int> image_analysis(const CImg<unsigned char>& image,
 int main() {
   CImg<unsigned char> image("examples/data/lena.jpg");
 
-  std::vector<int> res = image_analysis(image, 0);
+  std::vector<int> res = ImageAnalysis(image, 0);
 
   for (int x : res) {
     std::cout << x << std::endl;
