c23d6c3dd399f5083767a9f131f687e693814352
Jerson ZC <jzunigacoayla@gmail.com>
2021-10-01 05:08:20 -0500

    feat: rename variable res to hist
    Give better variable names

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@ using namespace cimg_library;
 // Calculate the histogram of an image region
 void Histogram(const CImg<unsigned char>& image, const int x0, const int y0,
                const int x1, const int y1, const int channel,
-               std::vector<int>& res) {
+               std::vector<int>& hist) {
   for (int i = x0; i < x1; i++) {
     for (int j = y0; j < y1; j++) {
-      res[image(i, j, channel)]++;
+      hist[image(i, j, channel)]++;
     }
   }
 }
@@ -19,31 +19,31 @@ void Histogram(const CImg<unsigned char>& image, const int x0, const int y0,
 // Returns pixel intensity on given channel
 std::vector<int> ImageAnalysis(const CImg<unsigned char>& image,
                                const int channel) {
-  std::vector<int> res(256, 0);
+  std::vector<int> hist(256, 0);
 
   const int width = image.width();
   const int height = image.height();
 
   // clang-format off
   Histogram(image, 0, 0, width / 4, height,
-            channel, res);
+            channel, hist);
   Histogram(image, (width / 4), 0, (width / 4) * 2, height,
-            channel, res);
+            channel, hist);
   Histogram(image, (width / 4) * 2, 0, (width / 4) * 3, height,
-            channel, res);
+            channel, hist);
   Histogram(image, (width / 4) * 3, 0, width, height,
-            channel, res);
+            channel, hist);
   // clang-format on
 
-  return res;
+  return hist;
 }
 
 int main() {
   CImg<unsigned char> image("examples/data/lena.jpg");
 
-  std::vector<int> res = ImageAnalysis(image, 0);
+  std::vector<int> hist = ImageAnalysis(image, 0);
 
-  for (int x : res) {
+  for (int x : hist) {
     std::cout << x << std::endl;
   }
 
