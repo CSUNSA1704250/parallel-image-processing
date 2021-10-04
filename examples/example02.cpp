5cfa34adcd84868361fce976945e94ea04d703f7
Jerson ZC <jzunigacoayla@gmail.com>
2021-10-04 08:52:23 -0500

    feat: remove examples folder

--- examples/example02.cpp
+++
@@ -1,8 +0,0 @@
-#include "CImg.h"
-using namespace cimg_library;
-
-int main() {
-  const CImg<float> img = CImg<float>("data/lena.jpg").histogram(256);
-  img.display_graph("My histogram", 2);
-  return 0;
-}
