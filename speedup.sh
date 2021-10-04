23090fb62e9fae92a68a377bbbae948253987238
Jerson ZC <jzunigacoayla@gmail.com>
2021-10-04 10:56:33 -0500

    feat: benchmark parallel.cpp on incremental threads

---
+++ speedup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+input_image="data/pexels-jess-loiterton-4783851.jpg"
+color="RED"
+num_threads=2
+
+results="speedup.csv"
+iterations=5
+
+echo "num_threads, time_ms" > $results
+for ((i = 1 ; i <= iterations ; i++)); do
+  ./parallel.out $input_image $color $num_threads >> $results
+  num_threads=$((num_threads*2))
+done
