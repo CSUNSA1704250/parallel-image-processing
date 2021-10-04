835e62071d66df82ec60cb1195513d764ae5dc42
Jerson ZC <jzunigacoayla@gmail.com>
2021-10-04 13:04:05 -0500

    fix: no space between data on csv

--- speedup.sh
+++ speedup.sh
@@ -5,9 +5,9 @@ color="RED"
 num_threads=2
 
 results="speedup.csv"
-iterations=5
+iterations=10
 
-echo "num_threads, time_ms" > $results
+echo "num_threads,time_ms" > $results
 for ((i = 1 ; i <= iterations ; i++)); do
   ./parallel.out $input_image $color $num_threads >> $results
   num_threads=$((num_threads*2))
