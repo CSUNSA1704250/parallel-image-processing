d9c3baeb68c9a04310a1b1d3761a88a40b9d1524
Jerson Zúñiga-Coayla <jzunigacoayla@gmail.com>
2021-10-07 22:08:57 -0500

    feat: add omp version

---
+++ src/omp.cpp
@@ -0,0 +1,76 @@
+#include <chrono>
+#include <iostream>
+#include <map>
+#include <vector>
+
+#include "CImg.h"
+
+using namespace cimg_library;
+
+// Calculate the histogram of an image region
+void Histogram(const CImg<unsigned char>& image, const int x0, const int y0,
+               const int x1, const int y1, const int channel,
+               std::vector<int>& hist) {
+  #pragma omp parallel for
+  for (int i = x0; i < x1; i++) {
+    for (int j = y0; j < y1; j++) {
+      #pragma omp atomic
+      hist[image(i, j, channel)]++;
+    }
+  }
+}
+
+// Returns pixel intensity on given channel
+std::vector<int> ImageAnalysis(const CImg<unsigned char>& image,
+                               const int channel) {
+  std::vector<int> hist(256, 0);
+
+  const int width = image.width();
+  const int height = image.height();
+
+  // clang-format off
+  Histogram(image, 0, 0, width / 4, height,
+            channel, hist);
+  Histogram(image, (width / 4), 0, (width / 4) * 2, height,
+            channel, hist);
+  Histogram(image, (width / 4) * 2, 0, (width / 4) * 3, height,
+            channel, hist);
+  Histogram(image, (width / 4) * 3, 0, width, height,
+            channel, hist);
+  // clang-format on
+
+  return hist;
+}
+
+int main(int argc, char* argv[]) {
+  if (argc == 3) {
+    std::map<std::string, int> colors{{"RED", 0}, {"GREEN", 1}, {"BLUE", 2}};
+
+    // Input image
+    CImg<unsigned char> image(argv[1]);
+    // Histogram color intensity
+    const int kColor = colors[argv[2]];
+
+    auto start = std::chrono::steady_clock::now();
+    std::vector<int> hist = ImageAnalysis(image, kColor);
+    auto end = std::chrono::steady_clock::now();
+
+    for (int x : hist) {
+      std::cout << x << std::endl;
+    }
+
+    auto diff = end - start;
+
+    std::cout << "Elapsed time in milliseconds: "
+              << std::chrono::duration<double, std::milli>(diff).count()
+              << std::endl;
+
+    return EXIT_SUCCESS;
+  } else {
+    std::cout << "Please follow template." << std::endl;
+    std::cout << "./main.out <input_image> <color>" << std::endl;
+
+    return EXIT_FAILURE;
+  }
+  return 0;
+}
