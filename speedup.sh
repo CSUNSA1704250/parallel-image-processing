496e06fc9e2dc045ba2108bf4fdaf0b037a354f3
Jerson ZC <jzunigacoayla@gmail.com>
2021-10-09 07:41:23 -0500

    fix: correct filename

--- speedup.sh
+++ speedup.sh
@@ -9,7 +9,7 @@ iterations=10
 
 echo "num_threads,time_ms" > $results
 for ((i = 1 ; i <= iterations ; i++)); do
-  ./parallel.out $input_image $color $num_threads >> $results
+  ./thread.out $input_image $color $num_threads >> $results
   num_threads=$((num_threads*2))
 done
 
